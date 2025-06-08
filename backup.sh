98a1ee07b0b8e473a959d71c4b4fb8484b2a47d3
Giovanni Toraldo <me@gionn.net>
2025-06-08 23:02:59 +0200

    add cleanup and more temporary files excluded

--- backup.sh
+++ backup.sh
@@ -1,4 +1,6 @@
 #!/bin/bash -e
 cd "$(dirname "$0")"
 source .envrc
-restic backup --exclude-file excludes.txt -v ~/
+restic backup --exclude-file excludes.txt -v ~/ -o s3.connections=10 --read-concurrency 4 --compression max --skip-if-unchanged
+restic forget --keep-daily 7 --keep-weekly 4 --keep-monthly 6
+restic prune
