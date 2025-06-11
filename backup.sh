0e184376b19983f62e814b3871eb48ca6f15554c
Giovanni Toraldo <me@gionn.net>
2025-06-11 22:49:26 +0200

    add path to restic

--- backup.sh
+++ backup.sh
@@ -1,4 +1,5 @@
 #!/bin/bash -e
+PATH=/home/gionn/bin:$PATH
 cd "$(dirname "$0")"
 source .envrc
 restic backup --exclude-file excludes.txt -v ~/ -o s3.connections=10 --read-concurrency 4 --compression max --skip-if-unchanged
