c77889b89fe8878dd31a3c136c8306dbc31481da
Giovanni Toraldo <me@gionn.net>
2025-06-11 22:53:58 +0200

    prune during forget

--- backup.sh
+++ backup.sh
@@ -3,5 +3,4 @@ PATH=/home/gionn/bin:$PATH
 cd "$(dirname "$0")"
 source .envrc
 restic backup --exclude-file excludes.txt -v ~/ -o s3.connections=10 --read-concurrency 4 --compression max --skip-if-unchanged
-restic forget --keep-daily 7 --keep-weekly 4 --keep-monthly 6
-restic prune
+restic forget --keep-daily 7 --keep-weekly 4 --keep-monthly 6 --prune
